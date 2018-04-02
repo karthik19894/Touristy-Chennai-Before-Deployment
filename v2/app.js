42d60bb13a71e1a5773bd8c7d42c47e2cb318bdb
Karthik Ruthiravelu <karthik19894@gmail.com>
2018-04-02 18:01:07 +0000

    Added Description to Each Place and Button to Display the Detailed information of the place

--- v2/app.js
+++ v2/app.js
@@ -9,7 +9,8 @@ mongoose.connect("mongodb://localhost/touristychennai");
 
 var campgroundSchema=new mongoose.Schema({
     name:String,
-    image:String
+    image:String,
+    description:String
 });
 
 var Campground=mongoose.model("Campground",campgroundSchema);
@@ -21,7 +22,25 @@ app.get('/',function(req,res){
     res.render("landing");
 });
 
+Campground.create({name:"Bessy Beach",image:"https://media.gettyimages.com/photos/good-morning-chennai-picture-id169401579",description:"An awesome beach located in one of the Calmest Places in Chennai "},function(err,place){
+    if(err){
+        console.log(err)
+    }
+    else
+    {
+        console.log("Place Created Successfully");
+    }
+});
 
+Campground.create({name:"Marina Beach",image:"https://media.gettyimages.com/photos/marina-beach-picture-id169352317",description:"No words to Say and the 2nd longest beach in the World "},function(err,place){
+    if(err){
+        console.log(err)
+    }
+    else
+    {
+        console.log("Place Created Successfully");
+    }
+});
 
 app.get('/campgrounds',function(req,res){
     Campground.find({},function(err,campgrounds){
@@ -40,7 +59,8 @@ app.get('/campgrounds',function(req,res){
 app.post('/campgrounds',function(req,res){
     var name=req.body.name;
     var image=req.body.image;
-    var campground={name:name,image:image};
+    var desc=req.body.description;
+    var campground={name:name,image:image,description:desc};
     Campground.create(campground,function(err,campground)
     {
         if(err){
@@ -61,6 +81,20 @@ app.get('/campgrounds/new',function(req,res)
     res.render('newcampground');
 });
 
+app.get('/campgrounds/:id',function(req,res){
+    var id=req.params.id;
+    Campground.findById(id,function(err,foundCampground){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render('show',{campground:foundCampground});
+            
+        }
+    });
+    
+    
+});
 
 app.listen(process.env.PORT,process.env.IP,function(){
     console.log("Yelp Camp Server has Started! :) ");
