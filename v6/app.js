6f734681553a36f8d9de56d9a50e664747fe70ce
Karthik Ruthiravelu <karthik19894@gmail.com>
2018-04-11 18:19:20 +0000

    Installed Packages Required For Authentication in v6

---
+++ v6/app.js
@@ -0,0 +1,115 @@
+var express=require("express");
+var app=express();
+var bodyParser=require('body-parser');
+var mongoose=require('mongoose');
+mongoose.connect("mongodb://localhost/touristychennaiv3");
+var Campground=require('./models/campground');
+var Comment=require('./models/comment');
+var seedDB=require('./seed');
+
+
+app.use(express.static(__dirname+'/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+
+app.set("view engine","ejs");
+
+seedDB();
+
+app.get('/',function(req,res){
+    res.render("landing");
+});
+
+
+app.get('/campgrounds',function(req,res){
+    Campground.find({},function(err,campgrounds){
+    if(err){
+        console.log("Error in fetching Camp grounds");
+    }
+    else
+    {
+        res.render('campgrounds/index',{campgrounds:campgrounds});
+    }
+    
+})
+});
+
+
+app.post('/campgrounds',function(req,res){
+    var name=req.body.name;
+    var image=req.body.image;
+    var desc=req.body.description;
+    var campground={name:name,image:image,description:desc};
+    Campground.create(campground,function(err,campground)
+    {
+        if(err){
+            console.log(err);
+        }
+        else
+        {
+            console.log("Campground created successfully");
+            res.redirect('/campgrounds');
+        }
+    });
+    
+    
+});
+
+app.get('/campgrounds/new',function(req,res)
+{
+    res.render('campgrounds/newcampground');
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    //find the campground with provided ID
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            //render show template with that campground
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+});
+
+app.get('/campgrounds/:id/comments/new',function(req,res){
+    var id=req.params.id;
+    Campground.findById(id,function(err,campground){
+        if(err){
+            console.log(err);
+        }
+        else
+        {
+            res.render('comments/new',{campground:campground});
+        }
+    })
+});
+
+app.post('/campgrounds/:id/comments',function(req,res){
+    //lookup campground using id
+    Campground.findById(req.params.id,function(err,campground){
+        if(err){
+            console.log(err);
+        }
+        Comment.create(req.body.comment,function(err,comment){
+            if(err){
+                console.log(err);
+            }
+            else
+            {
+                campground.comments.push(comment);
+                campground.save();
+                res.redirect('/campgrounds/'+campground._id);
+            }
+        })
+    });
+    //Create new comment
+    //Connect new comment to campground
+    //Redirect to campground show page
+});
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Yelp Camp Server has Started! :) ");
+});
+
